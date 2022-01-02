6781d836c1a7d4e4e626bde252f0410aa7a7457e
fullstackgjj <fullstackgjj@gmail.com>
2022-01-02 07:15:20 -0500

    Working product

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 raco-watcher
 ============
-README text here.
 
 To install this package onto your system:
 `$ git clone git@github.com:FullstackGJJ/raco-watcher.git`
 `$ cd raco-watcher`
 `$ raco pkg install`
+
+To auto re-run tests do
+`$ raco watcher -c "raco test <file/directory>"
+
+As simple as that
--- main.rkt
+++ main.rkt
@@ -1,17 +1,25 @@
 #lang racket/base
 
-(require file-watchers)
+(require (prefix-in fw: file-watchers))
 
 (module+ main
-  (require racket/cmdline
+  (require racket/format
            racket/list
-           racket/system
-           raco/command-name)
+           racket/string
+           (prefix-in cmd: racket/cmdline)
+           (prefix-in sys: racket/system)
+           (prefix-in cname: raco/command-name))
+
+  (define (format-paths paths)
+    (string-join
+      (map (lambda (p) (~a "-->  " p)) paths)
+      (format "~n")))
+
 
   (define cmd (make-parameter "world"))
-  (define path*
-    (command-line
-    #:program (short-program+command-name)
+  (define paths
+    (cmd:command-line
+    #:program (cname:short-program+command-name)
     #:once-each
     [("-c" "--command") c "command to run" (cmd c)]
     #:args target-path*
@@ -19,12 +27,26 @@
         (list (current-directory))
         (map string->path target-path*))))
 
-  (begin
-    (printf "Starting robust watch")
-    (with-handlers 
-      ([exn:break? (lambda (e) (printf "~nStopping...~n"))])
-      (thread-wait (watch path* (lambda (lst) (system (cmd)) displayln robust-watch)))
-      (displayln "All watchers are done."))))
+  (define does-not-exist (filter (λ (p) (not (fw:path-on-disk? p))) paths))
+  (define exists (filter fw:path-on-disk? paths))
+
+  (when (> (length does-not-exist) 0)
+    (printf
+      "These paths do not exist on the system and will not be monitored:~n~a~n~n"
+      (format-paths does-not-exist)))
+
+  (if (> (length exists) 0)
+    (begin
+      (printf "Starting robust watch")
+      (with-handlers
+        ([exn:break? (lambda (e) (printf "~nStopping...~n"))])
+        (thread-wait
+          (fw:watch
+            exists
+            (lambda (lst) (sys:system (cmd)))
+            displayln fw:robust-watch))
+        (displayln "All watchers are done.")))
+    (displayln "Nothing to watch. Exiting.")))
 
 (module+ test
   (require rackunit))
